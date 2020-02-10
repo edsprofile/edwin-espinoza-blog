be510e4e97f046ea434d24ad72ec84fa6bdd6e40
Edwin Espinoza <espined48@gmail.com>
2020-02-09 17:44:58 -0800

    Commit for deployment.

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app     = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("blog/landing")
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log("Blog server started!!!");
+});

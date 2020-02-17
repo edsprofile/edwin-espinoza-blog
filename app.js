efb59f40a7ad52a786701a280da1380fe251edec
Edwin Espinoza <espined48@gmail.com>
2020-02-16 20:21:32 -0800

    Refactored files, added footer and a picture of myself.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.render("blog/landing")
+    res.render("landing");
 });
 
 var port = process.env.PORT || 3000;
